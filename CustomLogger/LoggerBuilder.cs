33bd1208625204c9866c48b30034cdcec7ac5b47
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-08 17:12:57 +0300

    Started to implementing builder pattern

---
+++ CustomLogger/LoggerBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace CustomLogger
+{
+    public interface ILoggerBuilder
+    {
+        void SetName(string name);
+        void EnableTimestamp();
+        void AddErrorStream(TextWriter errorStream);
+        void AddWarningStream(TextWriter warningStream);
+        void AddInfoStream(TextWriter infoStream);
+        Logger GetResult();
+    }
+    public class LoggerBuilder : ILoggerBuilder
+    {
+        public void AddErrorStream(TextWriter errorStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddInfoStream(TextWriter infoStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddWarningStream(TextWriter warningStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnableTimestamp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Logger GetResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
