ccf22a3df31c0fad5d6b62036c52050a1abeb0c1
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-15 18:17:53 +0300

    Csv task - start;

---
+++ Csv/CsvEnumerable/CsvCollection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using CsvHelper;
+
+namespace CsvEnumerable
+{
+    public class CsvCollection<T> : IEnumerable<T>
+    {
+        protected CsvReader reader;
+        protected bool leaveReader;
+        private bool disposed = false;
+        public CsvCollection(TextReader reader, bool leaveReaderOpen = false)
+        {
+            this.reader = new CsvReader(reader);
+            this.leaveReader = leaveReaderOpen;
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                if (!leaveReader)
+                    reader.Dispose();
+                reader = null;
+                disposed = true;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CsvEnumerator<T>();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+
+    internal class CsvEnumerator<T>: IEnumerator<T>
+    {
+        protected CsvCollection<T> currentCollection;
+        protected T current;
+        private bool a = true;
+        private bool disposed = false;
+
+        object IEnumerator.Current
+        {
+            get => current;
+        }
+
+        T IEnumerator<T>.Current
+        {
+            get => current;
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+
+                disposed = true;
+            }
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            bool result = a;
+            a = !a;
+            return result;
+        }
+
+        void IEnumerator.Reset()
+        {
+            a = true;
+        }
+    }
+}
---
+++ Csv/CsvExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using CsvHelper;
+using CsvEnumerable;
+
+namespace CsvExample
+{
+    class SomeClass
+    {
+        public int Foo { get; set; }
+        public int Bar { get; set; } = 10;
+    }
+    class Program
+    {
+        static string filename = "./some.csv";
+        static void CreateFile()
+        {
+            List<SomeClass> someList = new List<SomeClass>
+            {
+                new SomeClass{ Foo = 10 },
+                new SomeClass{ Foo = 18 },
+                new SomeClass{ Foo = 13 },
+                new SomeClass{ Foo = 15 },
+                new SomeClass{ Foo = 17 },
+                new SomeClass{ Foo = 13 },
+                new SomeClass{ Foo = 14 },
+            };
+
+            using (CsvWriter writer = new CsvWriter(new StreamWriter(Path.GetFullPath(filename), false, Encoding.Unicode)))
+            {
+                writer.WriteRecords(someList);
+
+            }
+
+        }
+
+        static void ReadFile()
+        {
+            //using (CsvCollection<SomeClass> collection = new CsvCollection<SomeClass>(new StreamReader(Path.GetFullPath(filename), Encoding.Unicode)))
+            //{
+            //    foreach(var some in collection)
+            //    {
+            //        Console.WriteLine(some);
+            //    }
+            //}
+
+        }
+
+        static void Main(string[] args)
+        {
+            ReadFile();
+            Console.WriteLine("Press F to exit...");
+            Console.ReadKey();
+        }
+    }
+}
