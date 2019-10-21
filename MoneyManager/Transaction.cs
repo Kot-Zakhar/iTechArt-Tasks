58b6bb2c757321451637f52b2835f137377404a2
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-21 11:08:05 +0300

    Classes created

---
+++ MoneyManager/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyManager
+{
+    class Transaction
+    {
+        public Guid Id { get; protected set; }
+        public Guid CategoryId { get; protected set; }
+        public uint Amount { get; set; }
+        public DateTime Date { get; protected set; }
+        public Guid AssetId { get; protected set; }
+        public string Comment { get; set; }
+
+        
+    }
+}
