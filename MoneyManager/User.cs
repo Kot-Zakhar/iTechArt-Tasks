d7ea16a4cbf0845b82d32cf83d47cc6ab19f57f5
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-21 11:08:05 +0300

    Classes created

---
+++ MoneyManager/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyManager
+{
+    class User
+    {
+        public Guid id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Email { get; protected set; }
+        public string Hash { get; protected set; }
+        public string Salt { get; protected set; }
+
+    }
+}
