22e9a2146da6c80076ee55f38a72d45745cdc4aa
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-21 11:08:05 +0300

    Classes created

---
+++ MoneyManager/Asset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyManager
+{
+    class Asset
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; set; }
+        public Guid UserId { get; protected set; }
+    }
+}
---
+++ MoneyManager/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyManager
+{
+    enum CategoryType
+    {
+        All
+    }
+    class Category
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public CategoryType Type { get; protected set; }
+        public Guid ParentId { get; protected set; }
+    }
+}
