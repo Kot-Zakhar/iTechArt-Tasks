9717f021783c95681358a3038a540cf54548542c
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-22 18:57:46 +0300

    MoneyManager fake records generator done.

---
+++ MoneyManager/MoneyManager/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyManager
+{
+    public class Transaction
+    {
+        public Transaction()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Transaction(Guid Id)
+        {
+            this.Id = Id;
+        }
+        public Guid Id { get; protected set; }
+        public Category Category { get;  set; }
+        public double Amount { get; set; }
+        public DateTime Date { get;  set; }
+        public Asset Asset { get;  set; }
+        public string Comment { get; set; }
+
+        
+    }
+}
