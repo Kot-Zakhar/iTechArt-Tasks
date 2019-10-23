4de8f680bccd8b15720b22750da3851f38c18ae9
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-23 11:30:45 +0300

    Separating entities from DBContext

---
+++ MoneyManager/MoneyManager.ConsoleApproach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MoneyManager.Entity;
+using MoneyManager.DB;
+using MoneyManager.RandomGenerator;
+
+namespace ConsoleApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MoneyManagerContext())
+            {
+                User user = UserFaker.CreateUser();
+                Console.WriteLine($"Adding a user: {user.Id}\n{user.Name}\n{user.Email}\n");
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+            Console.WriteLine("hello");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MoneyManager/MoneyManager.DB/MoneyManagerDBInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MoneyManager.DB
+{
+    public class MoneyManagerDBInitializer : DropCreateDatabaseIfModelChanges<MoneyManagerContext>
+    {
+        protected override void Seed(MoneyManagerContext context)
+        {
+            base.Seed(context);
+
+            var generator = new RandomGenerator.MoneyManagerFaker();
+
+            generator.Generate();
+
+            context.Users.AddRange(generator.users);
+            context.Assets.AddRange(generator.assets);
+            context.Categories.AddRange(generator.categories);
+            context.Transactions.AddRange(generator.transactions);
+
+            context.SaveChanges();
+        }
+    }
+}
