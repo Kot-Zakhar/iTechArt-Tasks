9576490a4d868931948debce76e119738cbb13f5
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-23 10:42:02 +0300

    Beautified Faker.

--- MoneyManager/MoneyManager.RandomGenerator/UserFaker.cs
+++ MoneyManager/MoneyManager.RandomGenerator/UserFaker.cs
@@ -1,16 +1,33 @@
-﻿namespace MoneyManager.RandomGenerator
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace MoneyManager.RandomGenerator
 {
     public static class UserFaker
     {
+        private static RNGCryptoServiceProvider csp = new RNGCryptoServiceProvider();
+        private static int saltLength = 1024;
+        
         public static User CreateUser()
         {
+            byte[] saltBytes = new byte[saltLength * 2];
+            csp.GetBytes(saltBytes);
+            string salt = Convert.ToBase64String(saltBytes);
+            string name = Faker.Name.FullName();
+            string password = name;
+            string hash = Convert.ToBase64String(
+                SHA512.Create().ComputeHash(
+                    Encoding.Unicode.GetBytes(salt + password)
+                )
+            );
+            
             return new User()
             {
-                Email = Faker.Internet.Email(),
-                Name = Faker.Name.FullName(),
-                // todo: generate valid sha256 hash and salt
-                Salt = Faker.Lorem.GetFirstWord(),
-                Hash = Faker.Internet.Email()
+                Email = Faker.Internet.Email(name),
+                Name = name,
+                Salt = salt,
+                Hash = hash
             };
         }
 
