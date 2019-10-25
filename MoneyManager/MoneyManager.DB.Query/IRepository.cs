a91d27b43e6b2a4682eccb6e32eb69f2e34fa8cc
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-25 12:36:46 +0300

    Modifying repositories' interfaces; Implementing AssetRepo, Repo, TransRepo, UserRepo.

--- MoneyManager/MoneyManager.DB.Query/IRepository.cs
+++ MoneyManager/MoneyManager.DB.Query/IRepository.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace MoneyManager.Repository
 {
+    /// <summary>
+    /// Task: "For each entity write creating, updating and deleting commands."
+    /// Task: "For each entity write queries that will return the entity by identifier."
+    /// </summary>
     public interface IRepository<T> : IDisposable where T : class
     {
         IEnumerable<T> GetAll();
---
+++ MoneyManager/MoneyManager.MSSQLLocalDBRepository/TransactionRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using MoneyManager.Entity;
+using MoneyManager.Repository;
+using System.Data.Entity;
+
+namespace MoneyManager.MSSQLLocalDBRepository
+{
+    public class TransactionRepository : Repository<Transaction>, ITransactionRepository
+    {
+        public TransactionRepository(DbContext context) : base(context) {}
+
+        public int DeleteByAssetId(Guid assetId, DateTime startDate, DateTime endDate)
+        {
+            return base.typeSet.RemoveRange(
+                from transaction in base.typeSet
+                where transaction.Asset.Id == assetId && transaction.Date > startDate && transaction.Date < endDate
+                select transaction
+            ).Count();
+        }
+
+        public int DeleteByAssetIdInCurrentMonth(Guid assetId)
+        {
+            var now = DateTime.Now;
+            var from = new DateTime(now.Year, now.Month, 1);
+            var to = from.AddDays(DateTime.DaysInMonth(now.Year, now.Month));
+            return DeleteByAssetId(assetId, from, to);
+        }
+
+        public IEnumerable<TransactionInfo> GetInfoByAssetId(Guid assetId, DateTime startDate, DateTime endDate)
+        {
+            return (from transaction in base.typeSet
+                    where transaction.Asset.Id == assetId && transaction.Date > startDate && transaction.Date < endDate
+                    select transaction).Select(transaction => new TransactionInfo(transaction));
+        }
+
+    }
+}
