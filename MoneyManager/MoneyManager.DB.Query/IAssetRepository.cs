cadb2be9426c62895dae2c535d6dbe6780208fc3
Захар Кот Work <kot.zakhar@gmail.com>
2019-10-25 12:36:46 +0300

    Modifying repositories' interfaces; Implementing AssetRepo, Repo, TransRepo, UserRepo.

--- MoneyManager/MoneyManager.DB.Query/IAssetRepository.cs
+++ MoneyManager/MoneyManager.DB.Query/IAssetRepository.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using MoneyManager.Entity;
 
 namespace MoneyManager.Repository
 {
@@ -8,14 +8,41 @@ namespace MoneyManager.Repository
     {
         public Guid Id;
         public string Name;
+
+        public AssetInfo(Asset asset)
+        {
+            Id = asset.Id;
+            Name = asset.Name;
+        }
+    }
+    public struct AssetBalanceInfo
+    {
+        public AssetInfo AssetInfo;
         public double Balance;
     }
+    public struct AssetIncomeAndExpensesInfo
+    {
+        public AssetInfo AssetId;
+        public double Income;
+        public double Expenses;
+        public int Month;
+        public int Year;
+    }
+
     public interface IAssetRepository : IRepository<Entity.Asset>
     {
         public AssetInfo GetAssetInfoById(Guid assetId);
 
         public IEnumerable<AssetInfo> GetInfosByUserId(Guid userId);
-        public IEnumerable<AssetInfo> GetInfosByUserIdSorted(Guid userId, IComparer<AssetInfo> comparer);
+
+        /// <summary>
+        /// Write a query that will return the asset list for the selected user (userId) ordered by the asset’s name.
+        /// </summary>
         public IEnumerable<AssetInfo> GetInfosByUserIdSortedByAssetName(Guid userId);
+
+        /// <summary>
+        /// Ordered by Transaction.Date and grouped by month.
+        /// </summary>
+        public IEnumerable<AssetIncomeAndExpensesInfo> GetAssetIncomeAndExpensesInfos(Guid assetId, DateTime from, DateTime to);
     }
 }
--- MoneyManager/MoneyManager.MSSQLLocalDBRepository/UserRepository.cs
+++ MoneyManager/MoneyManager.MSSQLLocalDBRepository/UserRepository.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-using System.Text;
+using System.Data.Entity;
+using MoneyManager.Entity;
 using MoneyManager.Repository;
 
 namespace MoneyManager.MSSQLLocalDBRepository
 {
-    public class UserRepository : Repository<Entity.User>, IUserRepository
+    public class UserRepository : Repository<User>, IUserRepository
     {
+        public UserRepository(DbContext context) : base(context) {}
+
+        public User GetByEmail(string email)
+        {
+            return (from user in base.typeSet
+                    where user.Email == email
+                    select user).FirstOrDefault();
+        }
+
+        public IEnumerable<UserInfo> GetInfos()
+        {
+            return (from user in base.typeSet
+                    select user)
+                    .Select(user => new UserInfo(user));
+        }
+        
+        public IEnumerable<UserInfo> GetInfosSortedByName()
+        {
+            return GetInfos().OrderBy(userInfo => userInfo.Name);
+        }
     }
 }
