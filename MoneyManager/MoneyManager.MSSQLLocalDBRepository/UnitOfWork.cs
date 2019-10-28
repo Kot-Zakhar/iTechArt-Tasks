0a193502d62b88322f06cde787db145b5d8c861b
Kot-Zakhar <kot_zakhar@mail.ru>
2019-10-28 18:47:42 +0300

    Done with UnitOfWork

--- MoneyManager/MoneyManager.MSSQLLocalDBRepository/UnitOfWork.cs
+++ MoneyManager/MoneyManager.MSSQLLocalDBRepository/UnitOfWork.cs
@@ -111,12 +111,18 @@ namespace MoneyManager.MSSQLLocalDBRepository
 
         public IQueryable<AssetIncomeAndExpensesInfo> GetAssetIncomeAndExpensesInfos(Guid assetId, DateTime startDate, DateTime endDate)
         {
-            //return TransactionRepository.GetAll().Where(t => t.Asset.Id == assetId && t.Date >= startDate && t.Date <= endDate)
-            //    .GroupBy(t => t.Date.Month)
-            //    .Select(group => new AssetIncomeAndExpensesInfo()
-            //    {
-            //        Month = group.Key
-            //    });
+            Asset asset = AssetRepository.GetById(assetId);
+            return TransactionRepository.GetAll().Where(t => t.Asset.Id == assetId && t.Date >= startDate && t.Date <= endDate)
+                .GroupBy(t => new DateTime(t.Date.Year, t.Date.Month, 1))
+                .Select(group => new AssetIncomeAndExpensesInfo(){
+                    Month = group.Key.Month,
+                    Year = group.Key.Year,
+                    AssetId = new AssetInfo(asset),
+                    Expenses = group.Where(t => t.Category.Type == CategoryType.Expense)
+                                    .Aggregate(0.0, (value, transaction) => value + transaction.Amount),
+                    Income = group.Where(t => t.Category.Type == CategoryType.Income)
+                                  .Aggregate(0.0, (value, transaction) => value + transaction.Amount)
+                });
         }
 
         public IQueryable<AssetBalance> GetUserAssetsBalances(Guid userId)
@@ -126,22 +132,63 @@ namespace MoneyManager.MSSQLLocalDBRepository
 
         public UserBalance GetUserBalance(Guid userId)
         {
-            throw new NotImplementedException();
+            return new UserBalance()
+            {
+                Balance = GetUserAssetsBalances(userId).Aggregate(0.0, (value, assetBalance) => value + assetBalance.Balance),
+                UserInfo = new UserInfo(UserRepository.GetById(userId))
+            };          
         }
 
+        /// <summary>
+        /// Write a query that will return the total amount of all parent categories for the selected type of operation (Income or Expenses).
+        /// The result should include only categories that have transactions in the current month. 
+        /// In addition, you should order results descending by Transaction.Amount and then ordered them by Category.Name.
+        /// </summary>
         public IQueryable<CategoryAmountInfo> GetUserCategoryAmountInfosInCurrentMonth(Guid userId, CategoryType categoryType)
         {
-            throw new NotImplementedException();
+            int month = DateTime.Now.Month;
+            int year = DateTime.Now.Year;
+            int days = DateTime.DaysInMonth(year, month);
+            DateTime firstDay = new DateTime(year, month, 1);
+            DateTime lastDay = firstDay.AddDays(days);
+
+            return GetUserTransactionInfos(userId)
+                .Where(t => t.Date >= firstDay && t.Date <= lastDay && t.CategoryInfo.Type == categoryType && String.IsNullOrEmpty(t.CategoryInfo.ParentName))
+                .GroupBy(t => t.CategoryInfo)
+                .Select(transactionGroup => new CategoryAmountInfo()
+                {
+                    Category = transactionGroup.Key,
+                    Amount = transactionGroup.Aggregate(0.0, (value, transaction) => value + transaction.Amount)
+                });
         }
 
         public IQueryable<UserMonthIncomeAndExpensesInfo> GetUserMonthIncomeAndExpensesInfos(Guid userId, DateTime startDate, DateTime endDate)
         {
-            throw new NotImplementedException();
+            return GetUserTransactionInfos(userId)
+                .Where(t => t.Date >= startDate && t.Date <= endDate)
+                .GroupBy(t => new DateTime(t.Date.Year, t.Date.Month, 1))
+                .Select(transactionGroup => new UserMonthIncomeAndExpensesInfo()
+                {
+                    Month = transactionGroup.Key.Month,
+                    Year = transactionGroup.Key.Year,
+                    UserInfo = new UserInfo(UserRepository.GetById(userId)),
+                    Expenses = transactionGroup.Where(t => t.CategoryInfo.Type == CategoryType.Expense)
+                                .Aggregate(0.0, (value, transaction) => value + transaction.Amount),
+                    Income = transactionGroup.Where(t => t.CategoryInfo.Type == CategoryType.Income)
+                                .Aggregate(0.0, (value, transaction) => value + transaction.Amount),
+
+                });
         }
 
         public IQueryable<TransactionInfo> GetUserTransactionInfos(Guid userId)
         {
-            throw new NotImplementedException();
+            var assets = AssetRepository
+                .GetAll()
+                .Where(asset => asset.User.Id == userId);
+            return TransactionRepository
+                .GetAll()
+                .Where(t => assets.Any(a => a.Id == t.Asset.Id))
+                .Select(t => new TransactionInfo(t));
         }
 
 
