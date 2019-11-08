bb4489efe12d88dbbb3b32edb291c5c500b85f67
Захар Кот Work <kot.zakhar@gmail.com>
2019-11-08 14:40:54 +0300

    done with point controller

---
+++ CustomModelBinder/CustomModelBinder.Api/Controllers/LocationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomModelBinder.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomModelBinder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        // GET: api/location
+        [HttpGet]
+        public ActionResult<Point> Get([FromQuery] string coord)
+        {
+            int[] coordinates = coord.Split(",").Select(value => Convert.ToInt32(value)).ToArray();
+            if (coordinates.Length != 3)
+                return NotFound();
+            return new Point()
+            {
+                x = coordinates[0],
+                y = coordinates[1],
+                z = coordinates[2]
+            };
+        }
+
+
+    }
+}
--- CustomModelBinder/CustomModelBinder.Api/Models/Point.cs
+++ CustomModelBinder/CustomModelBinder.Api/Models/Point.cs
@@ -7,5 +7,8 @@ namespace CustomModelBinder.Api.Models
 {
     public class Point
     {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
     }
 }
