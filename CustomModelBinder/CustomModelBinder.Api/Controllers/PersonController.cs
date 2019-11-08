1aa2dacda3dd6ef24836830ff20be02d1d0e4615
Захар Кот Work <kot.zakhar@gmail.com>
2019-11-08 14:22:19 +0300

    person api is done

---
+++ CustomModelBinder/CustomModelBinder.Api/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomModelBinder.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomModelBinder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/Person
+        [HttpGet]
+        public ActionResult<Person> Get()
+        {
+            return new Person();
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{base64Id}", Name = "Get")]
+        public ActionResult<Person> Get(string base64Id)
+        {
+            return new Person(Guid.Parse(Encoding.UTF8.GetString(Convert.FromBase64String(base64Id))));
+        }
+    }
+}
---
+++ CustomModelBinder/CustomModelBinder.Api/Models/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using Bogus.DataSets;
+
+namespace CustomModelBinder.Api.Models
+{
+    public class Person
+    {
+        private void InitPerson(Bogus.Person person)
+        {
+            Website = person.Website;
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            FullName = person.FullName;
+            UserName = person.UserName;
+            Avatar = person.Avatar;
+            Email = person.Email;
+            DateOfBirth = person.DateOfBirth;
+            Phone = person.Phone;
+        }
+        public Person()
+        {
+            Id = Guid.NewGuid();
+            InitPerson(new Bogus.Person(seed: Id.GetHashCode()));
+        }
+
+        public Person(Guid id){
+            Id = id;
+            InitPerson(new Bogus.Person(seed: Id.GetHashCode()));
+        }
+
+        public Guid Id { get; set; }
+
+        public string Website { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+    }
+}
