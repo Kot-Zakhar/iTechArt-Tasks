79b63fca370f2f2af3700ebb7586167784a5cea2
Захар Кот Work <kot.zakhar@gmail.com>
2019-11-15 10:28:31 +0300

    Initial commit: basic api project and readme

---
+++ CustomJsonFormatter/CustomJsonFormatter.Api/Models/Profile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomJsonFormatter.Api.Models
+{
+    public class Profile
+    {
+    }
+}
