9dff8de816fe5e236a93c81763e2e23696dfbbb8
Захар Кот Work <kot.zakhar@gmail.com>
2019-11-26 17:51:06 +0300

    Init: custom csvReader is done

---
+++ CsvReader/CsvReader.Reader/CustomCsvReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CustomCsv
+{
+    public class CustomCsvReader
+    {
+        private readonly StreamReader _stream;
+        private IList<string> _headers = new List<string>();
+        private readonly CustomCsvReaderOptions _options;
+
+        public CustomCsvReader(StreamReader stream, CustomCsvReaderOptions options = null)
+        {
+            _stream = stream ?? throw new NullReferenceException();
+            _options = options ?? new CustomCsvReaderOptions();
+            if (_options.AreHeadersInFile)
+            {
+                ParseRowAsHeaders();
+            }
+            else
+            {
+                _headers = _options.Headers;
+                if (_options.AreHeadersRequired && _headers == null)
+                    throw new Exception("Headers are not provided.");
+            }
+        }
+
+        private void ParseRowAsHeaders()
+        {
+            _headers = _stream?.ReadLine()
+                .Split(',')
+                .Select(s => s.Trim())
+                .ToList();
+        }
+
+        public IDictionary<string, string> ReadRecord()
+        {
+            if (_options.AreHeadersRequired && _headers.Count == 0)
+                throw new Exception("No headers.");
+            return ReadValues()
+                .Select((value, index) => new KeyValuePair<string, string>(_headers[index], value))
+                .ToDictionary(keyValue => keyValue.Key, keyValue => keyValue.Value);
+        }
+
+        public IEnumerable<string> ReadValues()
+        {
+            return _stream.ReadLine()
+                .Split(',')
+                .Select(s => s.Trim());
+        }
+    }
+}
---
+++ CsvReader/CsvReader.Reader/CustomCsvReaderOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomCsv
+{
+    public class CustomCsvReaderOptions
+    {
+        public IList<string> Headers { get; set; } = null;
+
+        public bool AreHeadersInFile { get; set; } = true;
+        public bool AreHeadersRequired { get; set; } = true;
+    }
+}
