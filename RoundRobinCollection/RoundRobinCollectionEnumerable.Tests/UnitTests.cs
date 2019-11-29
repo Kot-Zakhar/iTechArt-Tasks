ad670a3824254dfade99e1cc619be145be82ec0b
Захар Кот Work <kot.zakhar@gmail.com>
2019-11-29 15:40:41 +0300

    2 extra tests

--- RoundRobinCollection/RoundRobinCollectionEnumerable.Tests/UnitTests.cs
+++ RoundRobinCollection/RoundRobinCollectionEnumerable.Tests/UnitTests.cs
@@ -9,6 +9,17 @@ namespace RoundRobinCollectionEnumerable.Tests
 {
     public class UnitTests
     {
+        private const int sourceLength = 10;
+        private IList<int> _source;
+        private IEnumerable<int> _enumerable;
+
+        [SetUp]
+        public void InitSource()
+        {
+            _source = Enumerable.Range(0, sourceLength).ToList();
+            _enumerable = new RoundRobinCollectionEnumerable<int>(_source);
+        }
+
         [Test]
         public void CollectionCreation_NullSourceCollection_ThrowsArgumentNullException()
         {
@@ -28,22 +39,65 @@ namespace RoundRobinCollectionEnumerable.Tests
         }
 
         [Test]
-        public void SingleIterator_SingleIteration_IteratesWell()
+        public void IterationCollection_SingleIteratorAndSingleIteration_IteratesWell()
         {
-            int amount = 10;
+            IEnumerator<int> iter = new RoundRobinCollectionEnumerable<int>(_source);
 
-            IList <int> source = Enumerable.Range(0, amount).ToList();
-
-            IEnumerator<int> iter = new RoundRobinCollectionEnumerable<int>(source);
-
-            var result = Enumerable.Range(0, amount)
+            var result = Enumerable.Range(0, sourceLength)
                 .Select(i =>
                 {
                     iter.MoveNext();
                     return iter.Current;
                 }).ToList();
 
-            Assert.AreEqual(source, result);
+            Assert.AreEqual(_source, result);
+        }
+
+        [Test]
+        public void IterationThroughCollection_SingleIteratorAndSeveralIterations_IteratesEndlessly()
+        {
+            int iterationAmount = 3;
+
+            List<int> expected = new List<int>();
+            for (int i = 0; i < iterationAmount; i++)
+                expected.AddRange(_source);
+;
+            var iter = _enumerable.GetEnumerator();
+
+            var actual = Enumerable.Range(0, sourceLength * iterationAmount)
+                .Select(i =>
+                {
+                    iter.MoveNext();
+                    return iter.Current;
+                }).ToList();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IterationThroughCollection_SeveralIterators_IteratesEndlessly()
+        {
+            int iteratorsAmount = 3;
+
+            IList<IEnumerator<int>> iterators = Enumerable.Range(0, iteratorsAmount)
+                .Select(i => _enumerable.GetEnumerator()).ToList();
+
+            List<int> expected = new List<int>();
+            for (int i = 0; i < iteratorsAmount; i++)
+                expected.AddRange(_source);
+
+            var actual = new List<int>();
+
+            for (int i = 0; i < sourceLength; i++)
+            {
+                foreach(var iter in iterators)
+                {
+                    iter.MoveNext();
+                    actual.Add(iter.Current);
+                }
+            }
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
