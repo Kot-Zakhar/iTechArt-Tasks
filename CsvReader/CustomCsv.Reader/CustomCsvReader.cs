b0804fa1596886157460676d867dd4a7058c42c9
Kot-Zakhar <kot_zakhar@mail.ru>
2019-12-04 10:47:08 +0300

    Csv reader test (11 task) (#13)
    * Init: custom csvReader is done
    
    * 4 tests are done
    
    * 7 tests are done and checked.
    
    * Removing AreHeadersRequired field.
    
    * Throwing exception if csv file is not normalized.
    
    * Adding own headers bug was found.
    3 extra tests was added.
    
    * Options' property renaming.
    
    * Moving Fake data generation logic into new class.
    
    * ReaderCreation_StreamIsNull_ThrowsException -> ReaderCreation_StreamIsNull_ThrowsArgumentNullException

---
+++ CsvReader/CustomCsv.Reader/CustomCsvReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CustomCsv
+{
+    public class CustomCsvReader
+    {
+        private readonly TextReader _stream;
+        private IList<string> _headers;
+
+        public CustomCsvReader(TextReader stream, CustomCsvReaderOptions options = null)
+        {
+            _stream = stream ?? throw new ArgumentNullException();
+            var currentOptions = options ?? new CustomCsvReaderOptions();
+            if (currentOptions.ReadHeaders)
+            {
+                ParseRowAsHeaders();
+                if (_headers == null)
+                    throw new Exception("File is empty. Cannot read headers.");
+
+                if (currentOptions.Headers != null)
+                {
+                    if (currentOptions.Headers.Count != _headers.Count)
+                        throw new Exception("Provided headers amount not equal to the amount of headers in file.");
+                    _headers = currentOptions.Headers;
+                }
+            }
+            else
+                _headers = currentOptions.Headers;
+        }
+
+        private void ParseRowAsHeaders()
+        {
+            _headers = _stream?.ReadLine()?
+                .Split(',')
+                .Select(s => s.Trim())
+                .ToList();
+        }
+
+        public IDictionary<string, string> ReadRecord()
+        {
+            if (_headers == null || _headers.Count == 0)
+                throw new Exception("No headers.");
+            IList<string> values = ReadValues();
+            if (values == null)
+                return null;
+            if (values.Count != _headers.Count)
+                throw new Exception("Values amount not equal headers amount.");
+            return values
+                .Select((value, index) => new KeyValuePair<string, string>(_headers[index], value))
+                .ToDictionary(keyValue => keyValue.Key, keyValue => keyValue.Value);
+        }
+
+        public IList<string> ReadValues()
+        {
+            return _stream?.ReadLine()?
+                .Split(',')
+                .Select(s => s.Trim())
+                .ToList();
+        }
+    }
+}
---
+++ CsvReader/CustomCsv.UnitTests/CustomCsvReaderTests.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CustomCsv.UnitTests
+{
+    [TestFixture]
+    public class CustomCsvReaderTests
+    {
+
+        [Test]
+        public void ReadValues_GoodStreamWithHeadersAndNoOptions_ReturnsListOfValues()
+        {
+            TextReader stream = FakeCsvGenerator.GenerateGoodTextReader(addHeaders: true);
+            var csvReader = new CustomCsvReader(stream);
+
+            IList<string> recordValues = csvReader.ReadValues();
+
+            Assert.AreEqual(FakeCsvGenerator.GenerateRecordValues(0), recordValues);
+        }
+
+        [Test]
+        public void ReadValues_GoodStreamAndNoHeaders_ReturnsListOfValues()
+        {
+            TextReader stream = FakeCsvGenerator.GenerateGoodTextReader();
+            var csvReader = new CustomCsvReader(stream, new CustomCsvReaderOptions() { ReadHeaders = false });
+
+            IList<string> recordValues = csvReader.ReadValues();
+
+            Assert.AreEqual(FakeCsvGenerator.GenerateRecordValues(0), recordValues);
+        }
+
+        [Test]
+        public void ReadRecord_GoodStreamWithHeadersAndNoOptions_ReturnsDictionary()
+        {
+            TextReader stream = FakeCsvGenerator.GenerateGoodTextReader(addHeaders: true);
+            var csvReader = new CustomCsvReader(stream);
+
+            IDictionary<string, string> record = csvReader.ReadRecord();
+
+            Assert.AreEqual(FakeCsvGenerator.GenerateRecord(FakeCsvGenerator.GenerateRecordValues(-1), FakeCsvGenerator.GenerateRecordValues(0)), record);
+        }
+
+        [Test]
+        public void ReadRecord_GoodStreamNoHeadersInFileOrInOptions_ThrowsException()
+        {
+            var csvReader = new CustomCsvReader(FakeCsvGenerator.GenerateGoodTextReader(), new CustomCsvReaderOptions() { ReadHeaders = false });
+            Assert.Catch(() =>
+            {
+                csvReader.ReadRecord();
+            });
+        }
+
+        [Test]
+        public void ReadRecord_HeadersAmountNotEqualValueAmount_ThrowsException()
+        {
+            var csvReader = new CustomCsvReader(FakeCsvGenerator.GenerateNotNormalizedTextReader(addHeaders: true));
+
+            Assert.Catch(() =>
+            {
+                var results = Enumerable.Range(0, FakeCsvGenerator.magicNumber).Select(i => csvReader.ReadRecord()).ToList();
+            });
+        }
+
+        [Test]
+        public void ReadRecord_ProvidingOwnHeadersInsteadHeadersInFile_ReturnsDictionary()
+        {
+            var csvReader = new CustomCsvReader(
+                FakeCsvGenerator.GenerateGoodTextReader(addHeaders: true),
+                new CustomCsvReaderOptions()
+                {
+                    ReadHeaders = true,
+                    Headers = FakeCsvGenerator.GenerateRecordValues(-10)
+                });
+
+            var result = Enumerable.Range(0, FakeCsvGenerator.magicNumber).Select(i => csvReader.ReadRecord()).ToList();
+
+            Assert.AreEqual(
+                Enumerable.Range(0, FakeCsvGenerator.magicNumber).Select(i => FakeCsvGenerator.GenerateRecord(FakeCsvGenerator.GenerateRecordValues(-10), FakeCsvGenerator.GenerateRecordValues(i))).ToList(),
+                result
+            );
+
+        }
+
+        [Test]
+        public void ReadRecord_AmountOfProvidedHeadersNotSameAsAmountOfHeadersInFile_ThrowsExceptionAtCreation()
+        {
+            Assert.Catch(() =>
+            {
+                var csvReader = new CustomCsvReader(
+                    FakeCsvGenerator.GenerateGoodTextReader(addHeaders: true),
+                    new CustomCsvReaderOptions()
+                    {
+                        ReadHeaders = true,
+                        Headers = FakeCsvGenerator.GenerateRecordValues(-10, FakeCsvGenerator.magicNumber * 2)
+                    });
+            });
+        }
+
+        [Test]
+        public void ReadRecord_ProvidingOwnHeadersWithNoHeadersInFile_ReturnsDictionary()
+        {
+            var csvReader = new CustomCsvReader(
+                FakeCsvGenerator.GenerateGoodTextReader(addHeaders: false, recordAmount: 10),
+                new CustomCsvReaderOptions()
+                {
+                    ReadHeaders = false,
+                    Headers = FakeCsvGenerator.GenerateRecordValues(-10)
+                });
+
+            var result = Enumerable.Range(0, 10).Select(i => csvReader.ReadRecord()).ToList();
+
+            Assert.AreEqual(
+                Enumerable.Range(0, 10).Select(i => FakeCsvGenerator.GenerateRecord(FakeCsvGenerator.GenerateRecordValues(-10), FakeCsvGenerator.GenerateRecordValues(i))).ToList(),
+                result
+            );
+        }
+
+        [Test]
+        public void ReadRecord_ReadingMoreThanExist_ReturnsNull()
+        {
+            var csvReader = new CustomCsvReader(FakeCsvGenerator.GenerateGoodTextReader(recordAmount: 4, addHeaders: true));
+
+            IList<IDictionary<string, string>> records = Enumerable.Range(0, 5).Select(index => csvReader.ReadRecord()).ToList();
+
+            Assert.IsNull(records[4]);
+        }
+
+        [Test]
+        public void ReaderCreation_StreamIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Catch(typeof(ArgumentNullException),() =>
+            {
+                new CustomCsvReader(null);
+            });
+        }
+
+
+
+    }
+}
