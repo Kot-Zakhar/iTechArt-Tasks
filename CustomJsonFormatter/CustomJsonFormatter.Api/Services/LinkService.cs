733110f7184f6bdf6c995d315eb8c5d79c7b4bfb
Kot-Zakhar <kot_zakhar@mail.ru>
2019-12-04 10:45:36 +0300

    Custom JSON Formatter (8 task) (#9)
    * Initial commit: basic api project and readme
    
    * Endpoints are done
    
    * Adding customJsonFormatter, wich simply wraps respose into 'Data' object
    
    * adding Article processing in jsonFormatter
    
    * Polymorphism fix
    
    * Formatter extension into new file; 'self-link' bug is fixed.
    
    * Fixing issues:
    extending IMvcBuilder instead of IServiceCollection;
    implementing Enumerable result processing;
    symplifying dictionary initialization;
    pattern matching was implemented;
    moving self-link adding into linkService.

---
+++ CustomJsonFormatter/CustomJsonFormatter.Api/Services/LinkService.cs
@@ -0,0 +1,41 @@
+﻿using CustomJsonFormatter.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomJsonFormatter.Api.Services
+{
+    public class LinkService
+    {
+        private string _host;
+        public LinkService(string host)
+        {
+            _host = host;
+        }
+        public IDictionary<string, string> GetLinks(Article article)
+        {
+            return new Dictionary<string, string>
+            {
+                ["get-author"] = _host + "/api/profile/" + article.AuthorId,
+                ["self"] = _host + "/api/article/" + article.Id
+            };
+        }
+        public IDictionary<string, string> GetLinks(Profile profile)
+        {
+            return new Dictionary<string, string>
+            {
+                ["self"] = _host + "/api/profile/" + profile.Id
+            };
+        }
+
+        public IDictionary<string, string> GetLinks(object obj)
+        {
+            if (obj is Profile profile)
+                return GetLinks(profile);
+            if (obj is Article article)
+                return GetLinks(article);
+            return new Dictionary<string, string>();
+        }
+    }
+}
---
+++ CustomJsonFormatter/CustomJsonFormatter.Api/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+using CustomJsonFormatter.Api.Formatters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace CustomJsonFormatter.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddControllers()
+                .AddCustomJsonOutputFormatter("https://localhost:5001",
+                    new JsonSerializerOptions() {PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection()
+               .UseRouting()
+               .UseAuthorization()
+               .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+        }
+    }
+}
